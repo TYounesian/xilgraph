280352fd3ec0ae5d2c707422918386bd1524a813
Steve Azzolin <steve.azzolin1@gmail.com>
2025-11-21 13:59:47 +0100

    Added GIN + Fixed GAT

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from torch import nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, GATConv, global_mean_pool
+from torch_geometric.nn import GCNConv, GATConv, GINConv, global_mean_pool, global_add_pool
 
 
 class GCN(nn.Module):
-    def __init__(self, in_dim=7, hidden=16, out_dim=2, dropout=0):
+    def __init__(self, in_dim=7, hidden=100, out_dim=2, dropout=0):
         super().__init__()
         self.conv1 = GCNConv(in_dim, hidden)
         self.conv2 = GCNConv(hidden, hidden)
@@ -24,7 +24,7 @@ class GCN(nn.Module):
 
 
 class GAT(nn.Module):
-    def __init__(self, in_dim=7,  hidden=16, heads1=4, heads2=4, out_dim=2, attn_dropout=0.2, feat_dropout=0.2):
+    def __init__(self, in_dim=7, hidden=100, heads1=4, heads2=4, out_dim=2, attn_dropout=0.0, feat_dropout=0.0):
         super().__init__()
         self.gat1 = GATConv(
             in_channels=in_dim,
@@ -37,23 +37,67 @@ class GAT(nn.Module):
             in_channels=hidden * heads1,
             out_channels=hidden,
             heads=heads2,
+            concat=True,
+            dropout=attn_dropout,
+        )
+        self.gat3 = GATConv(
+            in_channels=hidden * heads2,
+            out_channels=hidden,
+            heads=heads2,
             concat=False,
             dropout=attn_dropout,
         )
 
-        self.bn1 = nn.BatchNorm1d(hidden * heads1)
-        self.bn2 = nn.BatchNorm1d(hidden)
+        # self.bn1 = nn.BatchNorm1d(hidden * heads1)
+        # self.bn2 = nn.BatchNorm1d(hidden)
+        # self.bn3 = nn.BatchNorm1d(hidden)
 
         self.feat_dropout = nn.Dropout(feat_dropout)
         self.pool = global_mean_pool
         self.lin = nn.Linear(hidden, out_dim)
 
     def forward(self, x, edge_index, batch):
-        x = F.elu(self.bn1(self.gat1(x, edge_index)))
+        x = F.relu(self.gat1(x, edge_index))
         x = self.feat_dropout(x)
 
-        x = F.elu(self.bn2(self.gat2(x, edge_index)))
+        x = F.relu(self.gat2(x, edge_index))
+        x = self.feat_dropout(x)
+
+        x = F.relu(self.gat3(x, edge_index))
         x = self.feat_dropout(x)
 
         x = self.pool(x, batch)
+        return self.lin(x)
+    
+
+class GIN(nn.Module):
+    def __init__(self, in_dim=7, hidden=100, out_dim=2, dropout=0):
+        super().__init__()
+
+        nn1 = nn.Sequential(
+            nn.Linear(in_dim, hidden),
+            nn.ReLU(),
+        )
+        nn2 = nn.Sequential(
+            nn.Linear(hidden, hidden),
+            nn.ReLU(),
+        )
+        nn3 = nn.Sequential(
+            nn.Linear(hidden, hidden),
+            nn.ReLU(),                    
+        )
+        self.conv1 = GINConv(nn1)
+        self.conv2 = GINConv(nn2)
+        self.conv3 = GINConv(nn3)
+        self.dropout = nn.Dropout(dropout)
+        self.lin = nn.Linear(hidden, out_dim)
+
+    def forward(self, x, edge_index, batch):
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.dropout(x)
+        x = F.relu(self.conv2(x, edge_index))
+        x = self.dropout(x)
+        x = F.relu(self.conv3(x, edge_index))
+        x = self.dropout(x)
+        x = global_mean_pool(x, batch)
         return self.lin(x)
